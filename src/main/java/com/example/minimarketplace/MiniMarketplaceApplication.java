2d48892c4d0fd3d34bb92f90ecb888f97b1f8484
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-04-02 11:39:29 +0200

    init2

---
+++ src/main/java/com/example/minimarketplace/MiniMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.minimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minimarketplace/MiniMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
