1bed7c70a08d2e57b0dd31613870563fa7aa0ea8
alexandrusom <127322700+alexandrusom@users.noreply.github.com>
2024-04-02 13:42:41 +0200

    La till de 4 olika enum som används för produkt-klassen. La till tag för enum för enum-attribut i produkt-klassen

--- src/main/java/com/example/minimarketplace/model/Product/Product.java
+++ src/main/java/com/example/minimarketplace/model/Product/Product.java
@@ -1,4 +1,5 @@
-package com.example.minimarketplace.model;
+package com.example.minimarketplace.model.Product;
+import com.example.minimarketplace.model.User;
 import jakarta.persistence.*;
 
 import java.util.Date;
@@ -22,18 +23,28 @@ public class Product {
     @Column(name = "dateOfProduction")
     private Date dateOfProduction;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "type")
     private ProductType type;
 
    /* @Column(name = "picture")
     private Picture picture;
     */
+
     @Column(name = "price")
     private double price;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private ProductStatus status;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "color")
+    private ProductColor productColor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "condition")
+    private ProductCondition productCondition;
     public UUID getProductId() {
         return productId;
     }
---
+++ src/main/java/com/example/minimarketplace/model/Product/ProductColor.java
@@ -0,0 +1,15 @@
+package com.example.minimarketplace.model.Product;
+
+public enum ProductColor {
+    BLACK,
+    WHITE,
+    RED,
+    BLUE,
+    YELLOW,
+    ORANGE,
+    GREEN,
+    PURPLE,
+    GRAY,
+
+
+}
