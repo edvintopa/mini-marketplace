b21637845e5e9c73d067aeb6baa28db49c0c0369
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-04-09 11:34:09 +0200

    created ElectronicsType. holds different categories of electronics

---
+++ src/main/java/com/example/minimarketplace/model/dto/product/products/electronics/ElectronicsType.java
@@ -0,0 +1,22 @@
+package com.example.minimarketplace.model.dto.product.products.electronics;
+
+/**
+ * @author edvintopa
+ * @project mini-marketplace
+ * @created 2024-04-09
+ */
+public enum ElectronicsType {
+    PC,
+    LAPTOP,
+    SCREEN,
+    PERIPHERALS,
+    SMARTPHONE,
+    TABLET,
+    TV,
+    AUDIO,
+    CAMERA,
+    GAMING,
+    NETWORK,
+    SOFTWARE,
+    OTHER
+}
