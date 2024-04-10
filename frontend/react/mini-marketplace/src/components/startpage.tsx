768bbe430e0643963e9a07cd403636e850c68146
Valentino Glave <defliez@protonmail.com>
2024-04-11 01:50:04 +0200

    Add index.css, startpage.tsx, Navbar.tsx Refactor index.html, main.tsx, App.tsx

---
+++ frontend/react/mini-marketplace/src/components/startpage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../index.css";
+
+
+const StartPage = () => {
+  return (
+        <div className="w-full">
+        {/* <Navbar /> {} */}
+            <main className="main-content">
+                <div>
+                    <div className="banner">
+                        <h1>Mini Market</h1>
+                        <p>Explore our collection of amazing products</p>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+
+  );
+}
+
+export default StartPage;
+
--- frontend/react/mini-marketplace/src/main.tsx
+++ frontend/react/mini-marketplace/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.render(
+    <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
