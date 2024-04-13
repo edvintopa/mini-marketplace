99e6aee12db5061ced67532c7d19f810549053fc
Sam <114536657+genesick@users.noreply.github.com>
2024-04-13 23:41:54 +0200

    final update
    refactoring.
    är ej klar, har fortfarande problem med att komponenterna ska skicka ID mellan varandra.

--- frontend/react/mini-marketplace/src/components/ProductViewApp.tsx
+++ frontend/react/mini-marketplace/src/components/ProductViewApp.tsx
@@ -4,6 +4,7 @@ import "../CSS-files/productgallery.css";
 import { CurrentProductView } from './product-view/ProductView';
 
 export const ProductViewApp = ({id} : {id: string}) => {
+  console.log('id1: ' + id);
   return (
     <>
     <Navbar/>
