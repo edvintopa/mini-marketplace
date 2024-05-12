fc4cc2e2787229b210255dd90f06bfadc4763f90
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-05-13 00:03:18 +0200

    retrieves user details from repo during authentication

---
+++ src/main/java/com/example/minimarketplace/service/UserDetailsService.java
@@ -0,0 +1,41 @@
+package com.example.minimarketplace.service;
+
+import com.example.minimarketplace.controller.UserRepository;
+import com.example.minimarketplace.model.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @author edvintopa
+ * @project mini-marketplace
+ * @created 2024-05-12
+ */
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        List<String> roles = new ArrayList<>();
+        roles.add("USER");
+        UserDetails userDetails =
+                org.springframework.security.core.userdetails.User.builder()
+                        .username(user.getUsername())
+                        .password(user.getPassword())
+                        .roles(roles.toArray(new String[0]))
+                        .build();
+        return userDetails;
+    }
+}
