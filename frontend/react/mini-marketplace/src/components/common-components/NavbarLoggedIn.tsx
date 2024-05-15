a904b66f0949c477b0b2845f870cdd76e390a08b
Valentino Glave <defliez@protonmail.com>
2024-05-15 23:54:58 +0200

    feat(navbar): implement and style dropdown menu with profile and logout options
    Added a dropdown menu to the navbar for logged-in users, featuring 'View
    Profile' and 'Log out' options.

--- frontend/react/mini-marketplace/src/components/common-components/NavbarLoggedIn.tsx
+++ frontend/react/mini-marketplace/src/components/common-components/NavbarLoggedIn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../../CSS-files/index.css"; 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faShop, faCartShopping, faTimes, faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +10,30 @@ interface NavbarLoggedInProps {
 
 const NavbarLoggedIn: React.FC<NavbarLoggedInProps> = ({ toggleSavedProducts, isSavedProductsVisible }) => {
     console.log("Rendering NavbarLoggedIn");
+
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const toggleDropdown = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        setDropdownVisible(prev => !prev);
+    };
+
+
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setDropdownVisible(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
         <nav className="nav">
             <div className="nav-left">
@@ -23,8 +47,12 @@ const NavbarLoggedIn: React.FC<NavbarLoggedInProps> = ({ toggleSavedProducts, is
             <button className="toggle-saved-products-btn" onClick={toggleSavedProducts}>
                 {isSavedProductsVisible ? <FontAwesomeIcon icon = {faTimes} /> : <FontAwesomeIcon icon = {faHeart} />}
             </button>
-                <a href="#"><FontAwesomeIcon icon={faCartShopping} /></a>
-                <a href="/profile" className="nav-right" id="profileIcon"><FontAwesomeIcon icon={faUser} /></a>
+            <a href="#"><FontAwesomeIcon icon={faCartShopping} /></a>
+            <a href="#" onClick={(event) => toggleDropdown(event)} className="nav-right" id="profileIcon"><FontAwesomeIcon icon={faUser} /></a>
+            <div className={`dropdown-menu ${isDropdownVisible ? 'visible' : ''}`} ref={dropdownRef}>
+                <a href="/profile">View Profile</a>
+                <button onClick={() => console.log("Add logout functionality here")}>Logout</button>
+            </div>
             </div>
         </nav>
     );
