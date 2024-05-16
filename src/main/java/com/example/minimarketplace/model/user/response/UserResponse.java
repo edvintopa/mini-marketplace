bfe908ad31a5eccbb38405fa0302909c29315fab
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-05-16 17:53:42 +0200

    returns relevant user information, filters away sensitive information like password

---
+++ src/main/java/com/example/minimarketplace/model/user/response/UserResponse.java
@@ -0,0 +1,42 @@
+package com.example.minimarketplace.model.user.response;
+
+/**
+ * @author edvintopa
+ * @project mini-marketplace
+ * @created 2024-05-16
+ */
+public class UserResponse {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private double balance;
+
+    public UserResponse(String firstName, String lastName, String username, String email, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.balance = balance;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
