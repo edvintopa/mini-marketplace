e630ceff722ca14cc3ecfa29e1d359fd54081b45
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-05-16 11:46:09 +0200

    added docs

--- src/main/java/com/example/minimarketplace/controller/UserController.java
+++ src/main/java/com/example/minimarketplace/controller/UserController.java
@@ -53,10 +53,18 @@ public class UserController {
     }
 
     /**
-     * @author edvintopa
-     * Uses save method available in repo interface.
+     * This method handles the registration of a new user.
+     * It first checks if the provided username and email are already in use.
+     * If either the username or email is in use, it returns a BAD_REQUEST status with an appropriate message.
+     * If both the username and email are not in use, it creates a new User object with the provided details,
+     * hashes the provided password using BCrypt, and saves the new user in the repository.
+     * It then returns a CREATED status with the ID of the newly created user.
+     * If any exception occurs during the process, it returns an INTERNAL_SERVER_ERROR status.
      *
-     * Recieves user obj (JSON) and then checks availability of email and username
+     * @param user This is a request body parameter that contains the new user's details.
+     * @return ResponseEntity This returns a response entity that contains either the ID of the newly created user (in case of successful registration) or a BAD_REQUEST/INTERNAL_SERVER_ERROR status (in case of failed registration or any other exception).
+     * @throws Exception This is a general exception that can be thrown for various reasons.
+     * @author edvintopa
      */
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
@@ -89,12 +97,30 @@ public class UserController {
                     user.getEmail()
 
             ));
-            return new ResponseEntity<>(newUser.getUserId().toString() ,HttpStatus.CREATED);    //TODO: return auth token
+            return new ResponseEntity<>(newUser.getUserId().toString() ,HttpStatus.CREATED);    //TODO: return auth token and automatically log in after registration
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    /**
+     * This method handles the login request. It takes a LoginRequest object as input, which contains the username and password.
+     * The method attempts to authenticate the user with the provided credentials. If the authentication is successful, a JWT token is generated for the user.
+     * The token along with the username is then wrapped into a LoginResponse object and sent back to the client.
+     * <p>
+     * If the authentication fails due to bad credentials, an ErrorResponse object is created with a status of BAD_REQUEST and a message indicating invalid username or password.
+     * This ErrorResponse object is then sent back to the client.
+     * <p>
+     * If any other exception occurs during the process, an ErrorResponse object is created with a status of BAD_REQUEST and the exception message.
+     * This ErrorResponse object is then sent back to the client.
+     *
+     * @param loginRequest This is a request body parameter that contains the login credentials (username and password).
+     * @return ResponseEntity This returns a response entity that contains either a LoginResponse object (in case of successful authentication) or an ErrorResponse object (in case of failed authentication or any other exception).
+     * @throws BadCredentialsException This exception is thrown when the provided credentials are invalid.
+     * @throws Exception This is a general exception that can be thrown for various reasons other than bad credentials.
+     *
+     * @author edvintopa
+     */
     @ResponseBody
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public ResponseEntity login(@RequestBody LoginRequest loginRequest)  {
