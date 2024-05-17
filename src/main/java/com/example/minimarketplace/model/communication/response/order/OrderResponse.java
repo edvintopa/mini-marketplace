0af77ba5cf1ca0d2a9fff5cd9628d1cd04bf0e49
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-05-17 21:51:17 +0200

    created OrderResponse

---
+++ src/main/java/com/example/minimarketplace/model/communication/response/order/OrderResponse.java
@@ -0,0 +1,51 @@
+package com.example.minimarketplace.model.communication.response.order;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @author edvintopa
+ * @project mini-marketplace
+ * @created 2024-05-17
+ */
+public class OrderResponse {
+
+    private String sellerUsername;
+    private Date orderDate;
+    private double total;
+    private boolean isConfirmed;
+    private UUID productId;
+
+    public OrderResponse(String sellerUsername,
+                         Date orderDate,
+                         double total,
+                         boolean isConfirmed,
+                         UUID productId) {
+
+        this.sellerUsername = sellerUsername;
+        this.orderDate = orderDate;
+        this.total = total;
+        this.isConfirmed = isConfirmed;
+        this.productId = productId;
+    }
+
+    public String getSellerUsername() {
+        return sellerUsername;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public boolean isConfirmed() {
+        return isConfirmed;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+}
