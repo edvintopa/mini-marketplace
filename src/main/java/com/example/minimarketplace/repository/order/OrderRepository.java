5bc32396f3a4aa6226519154215da3e3857dce7f
edvintopa <55089202+edvintopa@users.noreply.github.com>
2024-05-17 21:51:05 +0200

    added findAllByBuyerId

---
+++ src/main/java/com/example/minimarketplace/repository/order/OrderRepository.java
@@ -0,0 +1,18 @@
+package com.example.minimarketplace.repository.order;
+
+import com.example.minimarketplace.model.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author edvintopa
+ * @project mini-marketplace
+ * @created 2024-05-17
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findAllByBuyerId(UUID userId);
+}
