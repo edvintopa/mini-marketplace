6a30147757d3fefad52def60d27ae3f301b0b824
Valentino Glave <defliez@protonmail.com>
2024-05-17 23:52:10 +0200

    update(create product): Add new fields in form
    Add new fields in form in RegisterProductCard.tsx to reflect enum in
    backend

--- frontend/react/mini-marketplace/src/components/register-product/RegisterProductCard.tsx
+++ frontend/react/mini-marketplace/src/components/register-product/RegisterProductCard.tsx
@@ -1,30 +1,45 @@
 import React, { useState } from 'react'
 import '../../CSS-files/index.css';
 
+const conditions = [
+    'NEW', 'VERY_GOOD', 'GOOD', 'NOT_WORKING_PROPERLY', 'USED',
+];
+
 const colors = [
     'BLACK', 'WHITE', 'RED', 'BLUE', 'YELLOW', 'ORANGE',
     'GREEN', 'PURPLE', 'GRAY', 'OTHER', 'PINK', 'OLIVE',
-    'NAVY', 'MAROON', 'BEIGE', 'BROWN'
+    'NAVY', 'MAROON', 'BEIGE', 'BROWN',
 ];
 
-const conditions = [
-    'NEW', 'VERY_GOOD', 'GOOD', 'NOT_WORKING_PROPERLY', 'USED'
+const seasons = [
+    'SPRING', 'SUMMER', 'AUTUMN', 'WINTER',
 ];
 
-const statuses = [
-  'AVAILABLE', 'NOT_AVAILABLE', 'ON_HOLD'
+const types = [
+    'TSHIRT', 'SHIRT', 'HOODIE', 'TROUSERS', 'SHORTS', 'SKIRT',
+    'DRESS', 'FOOTWEAR', 'HEADWEAR', 'ACCESSORY', 'OTHER',
 ];
+
+const sexes = [
+    'MEN', 'WOMEN', 'UNISEX',
+];
+
 
 export const RegisterProductCard: React.FC = () => {
     const [imageUrl, setImageUrl] = React.useState("https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg");
     const [color, setColor] = useState(colors[0]);
     const [condition, setCondition] = useState(conditions[0]);
-    const [status, setStatus] = useState(statuses[0]);  
+    const [type, setType] = useState(types[0]);  
+    const [season, setSeason] = useState(seasons[0]);
+    const [sex, setSex] = useState(sexes[0]);
     const [formData, setFormData] = useState({
-        name: '',
+        title: '',
+        description: '',
+        manufacturer: '',
         price: 0,
-        description: ''
     });
+
+    // color, condition, status
 
     const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -34,8 +49,8 @@ export const RegisterProductCard: React.FC = () => {
     };
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = event.target;
-        setFormData(prevData => ({ ...prevData, [name]: value }));
+        const { title, value } = event.target;
+        setFormData(prevData => ({ ...prevData, [title]: value }));
     };
 
     const handleColorChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -46,8 +61,16 @@ export const RegisterProductCard: React.FC = () => {
         setCondition(event.target.value);
     };
 
-    const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setStatus(event.target.value);
+    const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setType(event.target.value);
+    };
+
+    const handleSeasonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSeason(event.target.value);
+    };
+
+    const handleSexChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSex(event.target.value);
     };
 
     const postProduct = (event: React.FormEvent) => {
@@ -69,42 +92,62 @@ export const RegisterProductCard: React.FC = () => {
                 </div>
                 <div className="textFieldBox">
                     <h3 className="form-section-title">Name</h3>
-                    <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+                    <input type="text" title="title" value={formData.title} onChange={handleInputChange} />
                 </div>
                 <div className="textFieldBox">
                     <h3 className="form-section-title">Price</h3>
-                    <input type="number" name="price" value={formData.price} onChange={handleInputChange} />
+                    <input type="number" title="price" value={formData.price} onChange={handleInputChange} />
                 </div>
                 <div className="textFieldBox">
                     <h3 className="form-section-title">Description</h3>
-                    <input type="text" name="description" value={formData.description} onChange={handleInputChange} />
-                </div>
-                <div className="textFieldBox">
-                    <h3 className="form-section-title">Color</h3>
-                    <select value={color} onChange={handleColorChange} className="color-dropdown">
-                        {colors.map((colorOption) => (
-                            <option key={colorOption} value={colorOption}>
-                                {colorOption}
-                            </option>
-                        ))}
-                    </select>
+                    <input type="text" title="description" value={formData.description} onChange={handleInputChange} />
                 </div>
                 <div className="textFieldBox">
                     <h3 className="form-section-title">Condition</h3>
-                    <select value={condition} onChange={handleConditionChange} className="condition-dropdown">
+                    <select value={condition} onChange={handleConditionChange} className="dropdown">
                         {conditions.map((conditionOption) => (
                             <option key={conditionOption} value={conditionOption}>
-                                {conditionOption}
+                            {conditionOption}
                             </option>
                         ))}
                     </select>
                 </div>
                 <div className="textFieldBox">
-                    <h3 className="form-section-title">Status</h3>
-                    <select value={status} onChange={handleStatusChange} className="status-dropdown">
-                        {statuses.map((statusOption) => (
-                            <option key={statusOption} value={statusOption}>
-                                {statusOption}
+                    <h3 className="form-section-title">Color</h3>
+                    <select value={color} onChange={handleColorChange} className="dropdown">
+                        {colors.map((colorOption) => (
+                            <option key={colorOption} value={colorOption}>
+                            {colorOption}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="textFieldBox">
+                    <h3 className="form-section-title">Season</h3>
+                    <select value={season} onChange={handleSeasonChange} className="dropdown">
+                        {seasons.map((seasonOption) => (
+                            <option key={seasonOption} value={seasonOption}>
+                                {seasonOption}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="textFieldBox">
+                    <h3 className="form-section-title">Sex</h3>
+                    <select value={sex} onChange={handleSexChange} className="dropdown">
+                        {sexes.map((sexOption) => (
+                            <option key={sexOption} value={sexOption}>
+                                {sexOption}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="textFieldBox">
+                    <h3 className="form-section-title">Type</h3>
+                    <select value={type} onChange={handleTypeChange} className="dropdown">
+                        {types.map((typeOption) => (
+                            <option key={typeOption} value={typeOption}>
+                                {typeOption}
                             </option>
                         ))}
                     </select>
